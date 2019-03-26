3f67bbc5ee8ae4ad7e22f2b97ffa79d9679be4b5
Zerina <zfazlagic1@etf.unsa.ba>
2019-03-26 21:59:16 +0100

    Inicijalni projekat

---
+++ rent a car/src/main/java/com/rentacar/rent/a/car/RentACarApplication.java
@@ -0,0 +1,13 @@
+package com.rentacar.rent.a.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarApplication.class, args);
+	}
+
+}
---
+++ rent a car/src/test/java/com/rentacar/rent/a/car/RentACarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rentacar.rent.a.car;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentACarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
