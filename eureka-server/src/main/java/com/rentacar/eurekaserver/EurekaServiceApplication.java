42294089e1239d5720e66ab231811e2e2640add2
Dino <dcosic1@etf.unsa.ba>
2019-04-07 21:22:06 +0200

    Bug fixed

--- eureka-server/src/main/java/com/rentacar/eurekaserver/EurekaServiceApplication.java
+++ eureka-server/src/main/java/com/rentacar/eurekaserver/EurekaServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
 
 }
