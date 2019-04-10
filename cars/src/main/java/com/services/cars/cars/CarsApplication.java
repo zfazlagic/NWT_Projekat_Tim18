32b17a5fa9d290441eb7b862a8a4d6d3f0dcb7ae
Zerina <zfazlagic1@etf.unsa.ba>
2019-04-10 22:43:06 +0200

    prepravke za eureku

--- cars/src/main/java/com/services/cars/cars/CarsApplication.java
+++ cars/src/main/java/com/services/cars/cars/CarsApplication.java
@@ -8,10 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import java.util.List;
+import com.google.common.collect.ImmutableList;
+import org.springframework.http.MediaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
 
 @SpringBootApplication(scanBasePackages = {"com.services"})
 @EntityScan("com.cars.model")
@@ -33,6 +43,19 @@ public class CarsApplication {
 
 		};
 	}*/
+@Bean
+public RestTemplate restTemplate() {
+	RestTemplate restTemplate = new RestTemplate();
+	List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
+	for (HttpMessageConverter<?> converter : converters) {
+		if (converter instanceof MappingJackson2HttpMessageConverter) {
+			MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
+			jsonConverter.setObjectMapper(new ObjectMapper());
+			jsonConverter.setSupportedMediaTypes(ImmutableList.of(new MediaType("application", "json", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET), new MediaType("text", "javascript", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET)));
+		}
+	}
+	return restTemplate;
 
 
+}
 }
