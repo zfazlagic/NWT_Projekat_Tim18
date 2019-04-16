8856036e9440a7b93893ebc7a8f5df9ad020a4ba
Dino Cosic <dcosic1@etf.unsa.ba>
2019-04-16 21:59:12 +0200

    Rest template bux fixed, sync communication works

--- userActivities/src/main/java/com/userActivity/userActivities/UserActivitiesApplication.java
+++ userActivities/src/main/java/com/userActivity/userActivities/UserActivitiesApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -59,6 +60,7 @@ public class UserActivitiesApplication {
 //		};
 //	}
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
