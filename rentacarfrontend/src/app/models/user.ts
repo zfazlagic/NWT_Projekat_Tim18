d2a903146b5c33576aab3db7a4c2090362bef549
Djulsa Musinovic <toni.torlakovic@sandata.com>
2019-05-30 00:12:37 +0200

    register component initial

---
+++ rentacarfrontend/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    firstName: string;
+    lastName: string;
+    username:string;
+    email: string;
+    password: string;
+    isVerified: number;
+    role: string;
+    createdAt: Date;
+}
